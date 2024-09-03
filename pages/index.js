0ab7c026eb42d046c8ef0a189cb59795de2a732b
Rodrigo Borge <rodrigo@borge.com.br>
2024-09-03 01:17:03 +0000

    atualizacao titulo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Flavia, eu amo vc. Se vc me ama da uma risadinha.</h1>
+    return <h1>Flavia, eu amo vc. Segitt push vc me ama da uma risadinha.</h1>
 }
 
 export default Home;
